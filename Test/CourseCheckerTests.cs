234806fee6e43e218bde69fa55a5b65f110a0919
TorLindstrom <tor.lindstrom@winlas.se>
2024-12-02 17:15:42 +0100

    refactor: arrange/act/assert

--- Test/CourseCheckerTests.cs
+++ Test/CourseCheckerTests.cs
@@ -7,14 +7,20 @@ public class CourseCheckerTests
     [Fact(DisplayName = "If there is no navalfleet in the way, return true")]
     public void CheckCourse_NoNavalFleetInTheWay_ShouldReturnTrue()
     {
+        // Act
         bool courseIsClear = CourseChecker.CheckCourse(["X", "0"]);
+        
+        // Assert
         Assert.True(courseIsClear);
     }
     
     [Fact(DisplayName = "If there is a navalfleet in the way, return false")]
     public void CheckCourse_NavalFleetInTheWay_ShouldReturnFalse()
     {
+        // Act
         bool courseIsClear = CourseChecker.CheckCourse(["X", "N"]);
+        
+        // Assert
         Assert.False(courseIsClear);
     }
 }
