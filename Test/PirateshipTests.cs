4e0672742321cef2205ecb4d46144321fb4a832a
TorLindstrom <tor.lindstrom@winlas.se>
2024-12-02 16:58:48 +0100

    refactor: use object initializer instead

--- Test/PirateshipTests.cs
+++ Test/PirateshipTests.cs
@@ -8,12 +8,12 @@ public class PirateshipTests
     [Fact(DisplayName = "PirateShip should have a row")]
     public void PirateShip_ShouldHaveARow()
     {
-        // arrange
-        var pirateShip = new Pirateship();
-        
         // act
-        pirateShip.Row = 1;
-        
+        var pirateShip = new Pirateship
+        {
+            Row = 1
+        };
+
         // assert
         pirateShip.Row.Should().Be(1);
     }
